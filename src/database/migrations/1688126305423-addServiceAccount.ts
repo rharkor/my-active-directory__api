984cc1e386d59db84023159ac0757d35d36313aa
rharkor <louis@huort.com>
2023-07-06 20:08:11 +0200

    update: workflows

--- src/database/migrations/1688126305423-addServiceAccount.ts
+++ src/database/migrations/1688126305423-addServiceAccount.ts
@@ -1,14 +1,15 @@
-import { MigrationInterface, QueryRunner } from "typeorm";
+import { MigrationInterface, QueryRunner } from 'typeorm';
 
 export class AddServiceAccount1688126305423 implements MigrationInterface {
-    name = 'AddServiceAccount1688126305423'
+  name = 'AddServiceAccount1688126305423';
 
-    public async up(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.query(`CREATE TABLE "service_account" ("id" SERIAL NOT NULL, "name" character varying NOT NULL, "token" character varying NOT NULL, CONSTRAINT "UQ_1c270dede67ac9b2534a0846521" UNIQUE ("name"), CONSTRAINT "PK_2efb318de61f6487f806627dbd2" PRIMARY KEY ("id"))`);
-    }
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(
+      `CREATE TABLE "service_account" ("id" SERIAL NOT NULL, "name" character varying NOT NULL, "token" character varying NOT NULL, CONSTRAINT "UQ_1c270dede67ac9b2534a0846521" UNIQUE ("name"), CONSTRAINT "PK_2efb318de61f6487f806627dbd2" PRIMARY KEY ("id"))`,
+    );
+  }
 
-    public async down(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.query(`DROP TABLE "service_account"`);
-    }
-
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(`DROP TABLE "service_account"`);
+  }
 }
--- src/database/migrations/1688459556860-tokenUnique.ts
+++ src/database/migrations/1688459556860-tokenUnique.ts
@@ -1,14 +1,17 @@
-import { MigrationInterface, QueryRunner } from "typeorm";
+import { MigrationInterface, QueryRunner } from 'typeorm';
 
 export class TokenUnique1688459556860 implements MigrationInterface {
-    name = 'TokenUnique1688459556860'
+  name = 'TokenUnique1688459556860';
 
-    public async up(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.query(`ALTER TABLE "service_account" ADD CONSTRAINT "UQ_30601b5371100372bfc6923ce78" UNIQUE ("token")`);
-    }
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(
+      `ALTER TABLE "service_account" ADD CONSTRAINT "UQ_30601b5371100372bfc6923ce78" UNIQUE ("token")`,
+    );
+  }
 
-    public async down(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.query(`ALTER TABLE "service_account" DROP CONSTRAINT "UQ_30601b5371100372bfc6923ce78"`);
-    }
-
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(
+      `ALTER TABLE "service_account" DROP CONSTRAINT "UQ_30601b5371100372bfc6923ce78"`,
+    );
+  }
 }
