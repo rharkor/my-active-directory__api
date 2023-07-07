31dc5c5fefbef9b1c8ae85c72e3d13dbb74ff9b3
rharkor <louis@huort.com>
2023-07-08 00:13:54 +0200

    fix: delete scrip

--- src/database/scripts/delete-all.ts
+++ src/database/scripts/delete-all.ts
@@ -4,9 +4,9 @@ const deleteAll = async () => {
   if (process.env.ENV !== 'dev')
     throw 'You can delete all data only in dev environment';
   const connection = await databaseConfiguration.initialize();
-  const sqls: string[] = [];
+  const sqls: string[] = ['user', 'service_account', 'user_roles_role', 'role'];
   for (const sql of sqls) {
-    await connection.query(`DELETE FROM ${sql}`).catch((error) => {
+    await connection.query(`DELETE FROM "${sql}"`).catch((error) => {
       console.error(error);
     });
     console.log(`Deleted all data from ${sql}`);
