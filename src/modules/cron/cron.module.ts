31c1833bbbb8f5f73f904d04dab801ebb80fff3c
rharkor <louis@huort.com>
2023-07-09 15:44:05 +0200

    feat: cron clear files

--- src/modules/cron/cron.module.ts
+++ src/modules/cron/cron.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { CronService } from './cron.service';
+import User from '../users/entities/user.entity';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([])],
+  imports: [TypeOrmModule.forFeature([User])],
   providers: [CronService],
 })
 export class CronModule {}
--- src/modules/cron/cron.service.ts
+++ src/modules/cron/cron.service.ts
@@ -1,16 +1,43 @@
 import { Injectable, Logger } from '@nestjs/common';
 import { Cron } from '@nestjs/schedule';
+import User from '../users/entities/user.entity';
+import { Repository } from 'typeorm';
+import { InjectRepository } from '@nestjs/typeorm';
+import * as fs from 'fs';
+import * as path from 'path';
 
 @Injectable()
 export class CronService {
-  constructor() {
-    //
-  }
+  constructor(
+    @InjectRepository(User)
+    private readonly usersRepository: Repository<User>,
+  ) {}
 
   private readonly logger = new Logger(CronService.name);
 
+  async cleanUnused() {
+    this.logger.log('Cleaning unused files');
+    //? Clean all files that are not used by any users
+    const allUsersProfilePictures = await this.usersRepository
+      .createQueryBuilder('user')
+      .select("metadata->>'profilePicture'")
+      .where("metadata->>'profilePicture' IS NOT NULL")
+      .getRawMany();
+    const allFiles = fs.readdirSync('uploads');
+    const usedFiles = new Set<string>();
+    allUsersProfilePictures.forEach((filename) => {
+      if (filename) usedFiles.add(filename);
+    });
+    const unusedFiles = allFiles.filter((file) => !usedFiles.has(file));
+    unusedFiles.forEach((file) => {
+      fs.unlinkSync(path.join('uploads', file));
+    });
+    this.logger.log(`Cleaned ${unusedFiles.length} files`);
+  }
+
   @Cron('0 * * * *') // Every hour
+  //@Cron('*/10 * * * * *') // Every 10 seconds
   async handleCron() {
-    //
+    this.cleanUnused();
   }
 }
