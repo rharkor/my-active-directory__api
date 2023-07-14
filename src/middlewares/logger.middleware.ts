913b6944f9983f63dd9b75d790128489e097ff88
rharkor <louis@huort.com>
2023-07-14 11:46:35 +0200

    feat: debug log req user

--- src/middlewares/logger.middleware.ts
+++ src/middlewares/logger.middleware.ts
@@ -1,6 +1,8 @@
+import { PayloadType } from '@/types/auth';
 import { Injectable, NestMiddleware, Logger } from '@nestjs/common';
 
 import { Request, Response, NextFunction } from 'express';
+import jwtDecode from 'jwt-decode';
 
 @Injectable()
 export class LoggerMiddleware implements NestMiddleware {
@@ -10,6 +12,33 @@ export class LoggerMiddleware implements NestMiddleware {
     const { ip, method, originalUrl: url } = request;
     const userAgent = request.get('user-agent') || '';
 
+    let additionalInfo = '';
+    if (process.env.NODE_ENV !== 'production') {
+      const cookies = request.cookies;
+      const accessToken = cookies['mad-session'];
+      let decodedAccessToken: PayloadType | null = null;
+      try {
+        if (accessToken) decodedAccessToken = jwtDecode(accessToken);
+      } catch (error) {}
+      const refreshToken = cookies['mad-refresh'];
+      let decodedRefreshToken: PayloadType | null = null;
+      try {
+        if (refreshToken) decodedRefreshToken = jwtDecode(refreshToken);
+      } catch (error) {}
+
+      const bearerToken = request.get('authorization')?.split(' ')[1];
+      let decodedBearerToken: PayloadType | null = null;
+      try {
+        if (bearerToken) decodedBearerToken = jwtDecode(bearerToken);
+      } catch (error) {}
+
+      const user =
+        decodedAccessToken ?? decodedRefreshToken ?? decodedBearerToken;
+
+      additionalInfo +=
+        user?.email ?? user?.username ?? user?.id ?? 'anonymous';
+    }
+
     response.on('close', () => {
       const { statusCode } = response;
       const contentLength = response.get('content-length');
@@ -17,7 +46,7 @@ export class LoggerMiddleware implements NestMiddleware {
       this.logger.log(
         `${method} ${url} ${statusCode} ${
           contentLength ?? ''
-        } - ${userAgent} ${ip}`,
+        } - ${userAgent} ${ip} ${additionalInfo}`,
       );
     });
 
