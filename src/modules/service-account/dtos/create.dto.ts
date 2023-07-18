f064aa11d2d6873cad0ae472017758cf9fcf2031
rharkor <louis@huort.com>
2023-07-18 19:46:27 +0200

    fix: UA dtos

--- src/modules/service-account/dtos/create.dto.ts
+++ src/modules/service-account/dtos/create.dto.ts
@@ -1,18 +1,34 @@
 import { ApiProperty } from '@nestjs/swagger';
-import { IsNotEmpty, IsString } from 'class-validator';
+import {
+  IsNotEmpty,
+  IsOptional,
+  IsString,
+  Matches,
+  MaxLength,
+  MinLength,
+} from 'class-validator';
 
 export class CreateServiceAccountDto {
   @IsString()
   @IsNotEmpty()
+  @MinLength(5)
+  @MaxLength(50)
+  @Matches(/^[a-z0-9]+(?:-[a-z0-9]+)*$/, {
+    message: 'name must be kebab-case',
+  })
   @ApiProperty({
-    description: 'Name',
+    description: 'Name (kebab-case)',
     example: 'service-account',
     required: true,
+    minLength: 5,
+    maxLength: 50,
   })
   name: string;
 
   @IsString()
-  @IsNotEmpty()
+  @IsOptional()
+  @MinLength(1)
+  @MaxLength(150)
   @ApiProperty({
     description: 'Description',
     example: 'Service account for my app',
--- src/modules/service-account/dtos/update.dto.ts
+++ src/modules/service-account/dtos/update.dto.ts
@@ -1,18 +1,33 @@
 import { ApiProperty } from '@nestjs/swagger';
-import { IsOptional, IsString } from 'class-validator';
+import {
+  IsOptional,
+  IsString,
+  Matches,
+  MaxLength,
+  MinLength,
+} from 'class-validator';
 
 export class UpdateServiceAccountDto {
   @IsString()
   @IsOptional()
+  @MinLength(5)
+  @MaxLength(50)
+  @Matches(/^[a-z0-9]+(?:-[a-z0-9]+)*$/, {
+    message: 'name must be kebab-case',
+  })
   @ApiProperty({
-    description: 'Name',
+    description: 'Name (kebab-case)',
     example: 'service-account',
     required: false,
+    minLength: 5,
+    maxLength: 50,
   })
   name?: string;
 
   @IsString()
   @IsOptional()
+  @MinLength(1)
+  @MaxLength(150)
   @ApiProperty({
     description: 'Description',
     example: 'Service account for my app',
